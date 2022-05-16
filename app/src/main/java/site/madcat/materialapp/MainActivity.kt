e496347b9933b8d5d06d71b8131a54713082a702
Madcat51 <51@list.ru>
2022-05-16 14:09:56 +0300

    change themes

--- app/src/main/java/site/madcat/materialapp/MainActivity.kt
+++ app/src/main/java/site/madcat/materialapp/MainActivity.kt
@@ -1,27 +1,34 @@
 package site.madcat.materialapp
 
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
+import androidx.preference.PreferenceManager
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import site.madcat.materialapp.databinding.ActivityMainBinding
 import site.madcat.materialapp.ui.OtherFragment
 import site.madcat.materialapp.ui.PictureOfTheDayFragment
 
+
 class MainActivity : AppCompatActivity() {
     lateinit var bottomNavigationItemView: BottomNavigationView
     lateinit var binding: ActivityMainBinding
     private val fragmentManager: FragmentManager=supportFragmentManager
+    var sp: SharedPreferences?=null
+    private var savedTheme: Int=-1
+    private var oldTheme: Int=-1
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
+        checkProperties()
+        oldTheme=savedTheme
+        setTheme(savedTheme!!)
         binding=ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
@@ -29,7 +36,15 @@ class MainActivity : AppCompatActivity() {
             loadFragment(PictureOfTheDayFragment())
         }
         initNavigation()
+    }
 
+    fun checkProperties() {
+        sp=PreferenceManager.getDefaultSharedPreferences(this);
+        if (sp!!.getBoolean("themes", false) == true) {
+            savedTheme=R.style.Theme_MaterialApp2
+        } else {
+            savedTheme=R.style.Theme_MaterialApp
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -37,7 +52,24 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 
+    override fun onResume() {
+        checkProperties()
+        if (savedTheme != oldTheme) {
+            recreate()
+        }
+        super.onResume()
+    }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_settings -> {
+                oldTheme=savedTheme
+                val intent=Intent(this, SettingsActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
 
     fun initNavigation() {
         bottomNavigationItemView=binding.navView
@@ -47,7 +79,9 @@ class MainActivity : AppCompatActivity() {
                     loadFragment(PictureOfTheDayFragment())
                 }
                 R.id.navigation_other_fragment -> {
-                    loadFragment(OtherFragment())
+                    loadFragment(
+                        OtherFragment()
+                    )
                 }
             }; true
         }

c43a31ab0e536f36e6479a7d1601855a82c91a90
Madcat51 <51@list.ru>
2022-05-18 09:40:29 +0300

    lesson3

--- app/src/main/java/site/madcat/materialapp/MainActivity.kt
+++ app/src/main/java/site/madcat/materialapp/MainActivity.kt
@@ -35,6 +35,7 @@ class MainActivity : AppCompatActivity() {
         if (savedInstanceState == null) {
             loadFragment(PictureOfTheDayFragment())
         }
+
         initNavigation()
     }
 
